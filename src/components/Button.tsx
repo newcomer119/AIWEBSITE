68afb2d574460987b7b92d1a864bda7a072fc10d
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-08-31 19:30:56 +0530

    made changes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,22 @@
-export const Button = (props : React.PropsWithChildren) => {
+import { ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export const Button = ({ className = "", children, ...props }: ButtonProps) => {
   return (
-    <button className="relative py-2 px-4 rounded-lg font-medium text-sm text-white bg-gradient-to-b from-[#190d2e] to-[#4a208a] hover:from-[#1a0e30] hover:to-[#4e228f] transition-all duration-200">
+    <button 
+      className={`relative py-2 px-4 rounded-lg font-medium text-sm text-white bg-gradient-to-b from-[#190d2e] to-[#4a208a] hover:from-[#1a0e30] hover:to-[#4e228f] transition-all duration-200 ${className}`}
+      {...props}
+    >
       {/* Button borders and glow effects */}
       <div className="absolute inset-0 rounded-lg">
         <div className="border border-white/20 absolute inset-0 rounded-lg [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
         <div className="border absolute inset-0 border-white/40 rounded-lg [mask-image:linear-gradient(to_top,black,transparent)]"></div>
         <div className="absolute inset-0 shadow-[0_0_10px_rgb(140,69,255,.7)_inset] rounded-lg"></div>
       </div>
-      <span className="relative z-10">{props.children}</span>
+      <span className="relative z-10">{children}</span>
     </button>
   );
 };
